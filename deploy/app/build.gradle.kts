82d6e851c116da755b668064c345c1ba71fd7787
Angelo <aan@yanchware.com>
2025-02-06 22:30:10 +0100

    Fix issue with cosmos postgresql

--- deploy/app/build.gradle.kts
+++ deploy/app/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 dependencies {
     // Use JUnit Jupiter for testing.
     testImplementation(libs.junit.jupiter)
-    implementation("com.yanchware:fractal.sdk:12.0.5")
+    implementation("com.yanchware:fractal.sdk:12.0.8")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     // This dependency is used by the application.
